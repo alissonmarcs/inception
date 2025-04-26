5d7c382291e47b484b439a16658f845ad0a31404
Alisson Marcos <alissonmarcos249@gmail.com>
2025-04-26 15:31:50 -0300

    wip

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -11,6 +11,6 @@ then
     export $(cat /run/secrets/db_credentials)
     wp --allow-root config create  --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASS --dbhost=mariadb
     # wp --allow-root db create --path=/var/www/html/
-    # wp --allow-root core install  --url=almarcos.42.fr --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org
+    wp --allow-root core install  --url=almarcos.42.fr --title="demo" --admin_user=marcos --admin_password=123a123b --admin_email=alissonmarcos249@gmail.com
 fi
 exec php-fpm7.4 -F --fpm-config /wordpress.conf
