34c46ec9ff64207e9acf5f24e0533183cc51a0ff
Alisson <alissonmarcos249@gmail.com>
2025-04-26 10:41:53 -0300

    wip

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if grep  -q "database_name_here" /var/www/html/wp-config.php
+if ! command -v wp
 then
+    mkdir -p /run/php/
+    wget -O /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x /usr/local/bin/wp
+    cd /var/www/html/
+    rm -rf ./*
+    wp --allow-root core download
     export $(cat /run/secrets/db_credentials)
-    sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
-    sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
-    sed -i "s/password_here/$DB_USER_PASS/" /var/www/html/wp-config.php
-    sed -i "s/localhost/mariadb/" /var/www/html/wp-config.php
+    wp --allow-root config create  --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASS --dbhost=mariadb
+    # wp --allow-root db create --path=/var/www/html/
+    # wp --allow-root core install  --url=almarcos.42.fr --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org
 fi
 exec php-fpm7.4 -F --fpm-config /wordpress.conf
