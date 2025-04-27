37fc87eb4366374f92671c28b91f38f9001fabc9
Alisson <alissonmarcos249@gmail.com>
2025-04-27 17:57:57 -0300

    wip

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-if [ -d /var/lib/mysql/wordpress/ ]
+if [ ! -d /var/lib/mysql/wordpress/ ]
 then
-    exec mysqld_safe
-else
     service mariadb start 
-    export $(cat /run/secrets/db_credentials)
-    mysql -u root << EOF
+    export `cat /run/secrets/db_credentials`
+    mariadb << EOF
     CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;
     CREATE USER IF NOT EXISTS "$DB_USER"@'%' IDENTIFIED BY '$DB_USER_PASS';
     GRANT ALL ON \`$DB_NAME\`.* TO '$DB_USER'@'%';
     FLUSH PRIVILEGES;
 EOF
     service mariadb stop 
-    unset -v DB_NAME DB_USER DB_USER_PASS
-    exec mysqld_safe
 fi
+
+exec mysqld_safe
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -2,15 +2,15 @@
 
 if ! command -v wp
 then
-    mkdir -p /run/php/
     wget -O /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x /usr/local/bin/wp
     cd /var/www/html/
     rm -rf ./*
-    wp --allow-root core download
     export $(cat /run/secrets/db_credentials)
+    wp --allow-root core download
     wp --allow-root config create  --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASS --dbhost=mariadb
-    # wp --allow-root db create --path=/var/www/html/
-    wp --allow-root core install  --url=almarcos.42.fr --title="demo" --admin_user=marcos --admin_password=123a123b --admin_email=alissonmarcos249@gmail.com
+    wp --allow-root db create 
+    wp --allow-root core install  --url=almarcos.42.fr --title="42 - São Paulo" --admin_user=marcos --admin_password=123a123b --admin_email=alissonmarcos249@gmail.com
+    mkdir -p /run/php/
 fi
 exec php-fpm7.4 -F --fpm-config /wordpress.conf
