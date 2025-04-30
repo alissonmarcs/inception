047548a931931ab854ce9e4baec9d9e9753cc893
Alisson <alissonmarcos249@gmail.com>
2025-04-29 22:43:57 -0300

    done: no-admin user in wordpress db
    learned: when building containers, remove files inside folders
    that will be bind mounted in containers.
    
    learned: in host, always delete all files that will be bind mounted
    in containers.
    
    inside host, all is working fine, but in vm, when we will create an
    blog post, we still get the error message.

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,8 +9,8 @@ then
     export $(cat /run/secrets/db_credentials)
     wp --allow-root core download
     wp --allow-root config create  --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASS --dbhost=mariadb
-    wp --allow-root db create 
-    wp --allow-root core install  --url=almarcos.42.fr --title="42 - São Paulo" --admin_user=marcos --admin_password=123a123b --admin_email=alissonmarcos249@gmail.com
+    wp --allow-root core install  --url=almarcos.42.fr --title="42 - São Paulo" --admin_user=$ADMIN --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL
+    wp --allow-root user create $USER $USER_EMAIL --user_pass=$USER_PASS --role=author
     mkdir -p /run/php/
 fi
 exec php-fpm7.4 -F --fpm-config /wordpress.conf
