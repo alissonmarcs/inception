91c39f116769d12cfb44347fc6956c9e2a5645d0
Alisson <alissonmarcos249@gmail.com>
2025-04-30 20:53:08 -0300

    chmod -R 777 in wordpress folder

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -11,6 +11,7 @@ then
     wp --allow-root config create  --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASS --dbhost=mariadb
     wp --allow-root core install  --url=almarcos.42.fr --title="42 - São Paulo" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
     wp --allow-root user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASS --role=author
+    chmod -R 777 .
     mkdir -p /run/php/
 fi
 exec php-fpm7.4 -F --fpm-config /wordpress.conf
